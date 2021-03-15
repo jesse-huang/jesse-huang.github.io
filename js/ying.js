171c0b35efd770da9b2dbe1309dd05232a30b6a5
jesse <huanghaofengabc@gmail.com>
2021-03-15 23:51:43 +0800

    增加中英文切换

---
+++ js/ying.js
@@ -0,0 +1,12 @@
+var pageLang = {
+    "en": {
+        "collect": "Collect",
+        "workspace": "WorkSpace",
+        "contact": "Contact Us",
+    },
+    "zh": {
+        "collect": "作品集",
+        "workspace": "工作室",
+        "contact": "联系我们",
+    }
+};
